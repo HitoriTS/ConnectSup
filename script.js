0357e7b800f32b564293719912372b001ca0e012
HitoriTS <hitoritoketsushiro@gmail.com>
2025-10-08 17:16:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,32 @@ const listaChamados = document.getElementById("listaChamados");
 
 let chamados = JSON.parse(localStorage.getItem("chamados")) || [];
 
+function deletarChamado(i) {
+  if (confirm("Tem certeza que deseja arquivar este chamado?")) {
+    chamados[i].status = "Arquivado";
+    localStorage.setItem("chamados", JSON.stringify(chamados));
+    renderizarChamados();
+  }
+}
+
 function renderizarChamados() {
   listaChamados.innerHTML = "";
   chamados.forEach((c, i) => {
-    const div = document.createElement("div");
-    div.classList.add("ticket");
-    div.innerHTML = `
-      <strong>${c.empresa}</strong> → ${c.fornecedor}<br>
-      <em>${c.descricao}</em><br>
-      <small>Status: ${c.status}</small><br>
-      <button onclick="alterarStatus(${i})">✔ Concluir</button>
-      <button onclick="deletarChamado(${i})" style="background-color:#c10000; margin-left:5px;">✖ Deletar</button>
-    `;
-    listaChamados.appendChild(div);
+    if (c.status !== "Arquivado") { // só mostra chamados ativos
+      const div = document.createElement("div");
+      div.classList.add("ticket");
+      div.innerHTML = `
+        <strong>${c.empresa}</strong> → ${c.fornecedor}<br>
+        <em>${c.descricao}</em><br>
+        <small>Status: ${c.status}</small><br>
+        <button onclick="alterarStatus(${i})">✔ Concluir</button>
+        <button onclick="deletarChamado(${i})" style="background-color:#c10000; margin-left:5px;">✖ Arquivar</button>
+      `;
+      listaChamados.appendChild(div);
+    }
   });
 }
+
 
 // ----- NOVA FUNÇÃO: DELETAR CHAMADO -----
 function deletarChamado(i) {
